4ec718e15bd5e36f5a55cb2e2df70b92881fe5e7
Zywex <96013407+pwnedx@users.noreply.github.com>
2021-12-12 18:06:21 +0100

    Add files via upload

---
+++ RSA-tool/Window.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace RSA_tool
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.publicKeyBox = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.privateKeyBox = new System.Windows.Forms.RichTextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // publicKeyBox
+            // 
+            this.publicKeyBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.publicKeyBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.publicKeyBox.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.publicKeyBox.Location = new System.Drawing.Point(12, 48);
+            this.publicKeyBox.Name = "publicKeyBox";
+            this.publicKeyBox.Size = new System.Drawing.Size(272, 268);
+            this.publicKeyBox.TabIndex = 1;
+            this.publicKeyBox.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.publicKeyBox);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(297, 328);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Public Key Space";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.privateKeyBox);
+            this.panel2.Location = new System.Drawing.Point(315, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(297, 328);
+            this.panel2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Private Key Space";
+            // 
+            // privateKeyBox
+            // 
+            this.privateKeyBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.privateKeyBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.privateKeyBox.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.privateKeyBox.Location = new System.Drawing.Point(12, 48);
+            this.privateKeyBox.Name = "privateKeyBox";
+            this.privateKeyBox.Size = new System.Drawing.Size(272, 268);
+            this.privateKeyBox.TabIndex = 1;
+            this.privateKeyBox.Text = "";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel3.Controls.Add(this.generateButton);
+            this.panel3.Controls.Add(this.decryptButton);
+            this.panel3.Controls.Add(this.encryptButton);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.textBox);
+            this.panel3.Location = new System.Drawing.Point(12, 346);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(600, 186);
+            this.panel3.TabIndex = 3;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generateButton.Location = new System.Drawing.Point(399, 137);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(190, 37);
+            this.generateButton.TabIndex = 6;
+            this.generateButton.Text = "Generate Pair";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.decryptButton.Location = new System.Drawing.Point(205, 137);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(189, 37);
+            this.decryptButton.TabIndex = 5;
+            this.decryptButton.Text = "Decrypt";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            this.decryptButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.encryptButton.Location = new System.Drawing.Point(10, 137);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(190, 37);
+            this.encryptButton.TabIndex = 4;
+            this.encryptButton.Text = "Encrypt";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            this.encryptButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(157, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(286, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Text to Encrypt/Decrypt";
+            // 
+            // textBox
+            // 
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox.Location = new System.Drawing.Point(12, 48);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(575, 76);
+            this.textBox.TabIndex = 1;
+            this.textBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 546);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "RSA Tool";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox publicKeyBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox privateKeyBox;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox textBox;
+        private System.Windows.Forms.Button decryptButton;
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.Button generateButton;
+    }
+}
+
---
+++ RSA-tool/Window.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSA_tool
+{
+    public partial class Window : Form
+    {
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        public string RSAEncrypt(string public_key, string text)
+        {
+            using (var RSA = new RSACryptoServiceProvider(1024))
+            {
+                try
+                {              
+                    RSA.FromXmlString(public_key);
+                    var RSAEncode = RSA.Encrypt(Encoding.UTF8.GetBytes(text), true);
+                    var B64Encode = Convert.ToBase64String(RSAEncode);
+                    return B64Encode;
+                }
+                finally
+                {
+                    RSA.PersistKeyInCsp = false;
+                }
+            }
+        }
+
+        public string RSADecrypt(string private_key, string text)
+        {
+            using (var RSA = new RSACryptoServiceProvider(1024))
+            {
+                try
+                {
+                    RSA.FromXmlString(private_key);
+                    var RSADecode = Encoding.UTF8.GetString(RSA.Decrypt(Convert.FromBase64String(text), true));
+                    return RSADecode.ToString();
+                }
+                finally
+                {
+                    RSA.PersistKeyInCsp = false;
+                }
+            }
+        }
+
+        public void RSAGenerate()
+        {
+            using (var RSA = new RSACryptoServiceProvider(1024))
+            {
+                privateKeyBox.Text = RSA.ToXmlString(true);
+                publicKeyBox.Text = RSA.ToXmlString(false);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox.Text = RSAEncrypt(publicKeyBox.Text, textBox.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            RSAGenerate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox.Text = RSADecrypt(privateKeyBox.Text, textBox.Text);
+        }
+    }
+}
